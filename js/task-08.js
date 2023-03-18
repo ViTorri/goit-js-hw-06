5c4f2399f40fe26ce5603169f09893a569fc1237
ViTorri <45861551+ViTorri@users.noreply.github.com>
2023-03-18 18:09:26 +0000

    8 task is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,28 +1,41 @@
 const formRef = document.querySelector('.login-form');
 console.log(formRef);
 
-
 formRef.addEventListener('submit', onFormSubmit);
 formRef.addEventListener('submit', isInputEmpty);
+formRef.addEventListener('submit', isInputFilled);
+
+let loginFormData = {};
 
 function onFormSubmit(event) {
     event.preventDefault();
+
+    const elementEmail = event.currentTarget.elements.email.value;
+    const elementPassword = event.currentTarget.elements.password.value;
+
+    loginFormData = {
+        elementEmail,
+        elementPassword,
+    };
 }
 
 const inputsRef = document.querySelectorAll('input');
-    console.log(inputsRef);
-
+console.log(inputsRef);
+    
 function isInputEmpty() {
-    Array.from(inputsRef).every(input => {
-        if (input.value === "") {
-         alert("Все поля должны быть заполнены.");
-        }
-    })
+    const isEmpty = Array.from(inputsRef).some(input => !input.value);
+    if (isEmpty) {
+        alert("Все поля должны быть заполнены.");
+    }
 }
 
-    // const getElementEmail = event.currentTarget.elements.email.value;
-    // const getElementPassword = event.currentTarget.elements.password.value;
+function isInputFilled() {
+    const isFilled = Array.from(inputsRef).every(input => input.value);
+    if (isFilled) {
+        console.log(loginFormData);
+    }
+    formRef.reset();
+}
 
-    // console.log(getElementEmail);
-    // console.log(getElementPassword);
+
 
